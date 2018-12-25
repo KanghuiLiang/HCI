f443033d2ec01c4ab377ab08ffe53228c82267da
KanghuiLiang <kanghuihuiliang@gmail.com>
2018-12-26 04:47:13 +0700

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div
+      :style="{
+        'background-image': 'url(' + require('@/assets/kids-nature1.jpg') + ')'
+      }"
+      class="background-img"
+    ></div>
   </div>
 </template>
+
+<style scoped>
+.background-img {
+  transform: translateY(-60px);
+  display: table;
+  height: 100%;
+  width: 100%;
+  position: relative;
+  background-size: cover;
+}
+</style>
