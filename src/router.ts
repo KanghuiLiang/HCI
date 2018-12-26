900b0684ece4e3f3e7ff518fd633f99ed535164e
KanghuiLiang <kanghuihuiliang@gmail.com>
2018-12-26 23:10:54 +0700

    contact

--- src/router.ts
+++ src/router.ts
@@ -19,6 +19,13 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
+    },
+
+      {
+          path: "/contact-us",
+          name: "Contact Us",
+          component: () =>
+              import(/* webpackChunkName: "about" */ "./views/ContactUs.vue")
+      }
   ]
 });
---
+++ src/views/ContactUs.vue
@@ -0,0 +1,11 @@
+<template>
+  <div><form/></div>
+</template>
+
+<script>
+import form from "../components/contactForm.vue";
+export default {
+  name: "contact",
+  components: { form }
+};
+</script>
